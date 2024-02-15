8aca64c71597a9b5369b5924800b415a14dab75f
dust9629 <155696271+dust9629@users.noreply.github.com>
2024-02-15 23:40:22 +0900

    add script and style

--- mini_project09/js/script.js
+++ mini_project09/js/script.js
@@ -1,1 +1,8 @@
-const test = document.querySelector('body')
+let inputBox = document.getElementById("input-box");
+let resultBox = document.getElementById("result-box");
+let changeColor = () => {
+    let input = inputBox.value;
+    resultBox.style.backgroundColor = input;
+};
+inputBox.addEventListener("input", changeColor);
+window.addEventListener("load", changeColor)
